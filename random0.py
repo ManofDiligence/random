cf2ec2d2c8626fa13602e9a6e40b1b54163bc009
vincent <chiwang1712@gmail.com>
2021-08-22 18:48:27 +0800

    add variables

--- random0.py
+++ random0.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1
 	num = input('please guess the number:')
 	num = int(num)
 	if num == r:
 		print('the answer is correct!')
+		print('this is ur ',count,'time to guess the number.')
 		break
 	elif num > r:
 		print('This number is bigger than the answer')
 	elif num < r:
 		print('smaller than the ans')
+	print('this is ur ',count,'time to guess the number.')
